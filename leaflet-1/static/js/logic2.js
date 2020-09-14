0fa734ff04b9c0490d006bda1e38edb5f27714eb
Shawn Williams <swillia75@gmail.com>
2020-09-14 12:02:46 -0400

    added legend

---
+++ leaflet-1/static/js/logic2.js
@@ -0,0 +1,128 @@
+  //URL for all earthquakes for last 7days
+
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+//Perform get request to url and call createMarkers function
+
+d3.json(url, function(data) {   
+
+  //Pull out feature objects of data array
+
+  var features = data.features
+
+  console.log(features);
+
+  var quakeMarkers = [];
+  //Loop through features array to create markers
+
+  for (i = 0; i < features.length; i++) {
+
+    var color = " "
+
+    if (features[i].properties.mag > 7) {
+      color = "red";
+    }
+    else if (features[i].properties.mag  > 6) {
+     color = "orange";
+    }
+    else if (features[i].properties.mag  > 5) {
+      color = "yellow";
+    }
+    else if (features[i].properties.mag  > 4) {
+      color = "brown";
+    }
+    else if (features[i].properties.mag  > 3) {
+      color = "blue";
+    }
+    else if (features[i].properties.mag  > 2) {
+      color = "purple";
+    }
+    else if (features[i].properties.mag  > 1) {
+      color = "green";
+    }
+    else {
+      color = "black";
+    };
+    //Create circle markers and call selectColor and selectRadius for each feature
+
+    var quakeMarker = L.circle([features[i].geometry.coordinates[0], features[i].geometry.coordinates[1]], {
+      fillOpacity: 0.75,
+      color: "white",
+      fillColor: color,   
+      radius: features[i].properties.mag *15000
+
+      //Create popup for each marker
+
+    }).bindPopup("<h3>" + features[i].properties.place +
+    "</h3><h3>Date/Time:" + new Date(features[i].properties.time) + 
+    "</h3><h3>Magnitude:" + (features[i].properties.mag) + "</h3>");    
+
+    quakeMarkers.push(quakeMarker);
+  
+  };
+  console.log(quakeMarkers);
+
+  var quakes = L.layerGroup(quakeMarkers);
+
+  //Add streetmap layer
+    
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
+
+  var baseMaps = {
+    "Street Map": streetmap
+  };
+
+  var overlayMaps = {
+    "Earthquakes": quakes
+  };
+
+  //Create basemap
+
+  var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layer: [streetmap, quakes]
+  });
+
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsable: false
+  }).addTo(myMap);
+
+  //Create Legend
+
+  var legend = L.control({position: "bottomright"});
+  legend.onAdd = function(map) {
+    var div = L.DomUtil.create("div", "info legend");
+    var header = "<h1>Magnitude</h1>"
+    var magnitude = ["Magnitude", "7", "6", "5", "4", "3", "2", "1", "<1"];
+    var colorLabels = ["white", "red", "orange", "yellow", "brown", 
+                      "blue", "purple", "green", "black"];
+    
+                    
+
+    for (var i = 0; i < magnitude.length; i++) {
+    
+      div.innerHTML += 
+        "<li \"magnitude\">" + magnitude[i] + "</li>" +
+        "<li style = \"background-color:" + colorLabels[i] + "\">"+ "</li>" + 
+        "</div>";
+    
+
+    };
+    return div;
+    
+    
+  };
+  legend.addTo(myMap);
+});
+
+
